dcd6a4032d409f2b40794c5b937e2c561c8af2fd
Rgsantos12 <92592853+Rgsantos12@users.noreply.github.com>
2025-07-30 18:46:28 +0100

    updated training 1 speaker script and applied changes to AnalyzeDataset notebook to check the dataset im working with

--- train_vits_tts_phonemes_ptpt.py
+++ train_vits_tts_phonemes_ptpt.py
@@ -8,23 +8,22 @@ from TTS.tts.configs.shared_configs import BaseDatasetConfig
 from TTS.tts.configs.vits_config import VitsConfig
 from TTS.tts.datasets import load_tts_samples
 from TTS.tts.models.vits import Vits, VitsArgs, VitsAudioConfig
-from TTS.tts.utils.languages import LanguageManager
-from TTS.tts.utils.speakers import SpeakerManager
 from TTS.tts.utils.text.tokenizer import TTSTokenizer
 from TTS.utils.audio import AudioProcessor
+
 
 def main():
     print(f"Check cuda availability: {torch.cuda.is_available()}")
 
     print("Starting training script...")
-    output_path = "models/fine-tuned/one-voice"
+    output_path = "models/trained/one-voice"
 
     dataset_config = BaseDatasetConfig(
-        formatter="ljspeech", meta_file_train="metadata.txt", path=os.path.join("data", "in-one-speaker", "EUmember_96976")
+        formatter="ljspeech", meta_file_train="metadata.txt", path=os.path.join("data", "in-one-speaker", "EUmember_4466")
     )
 
     audio_config = VitsAudioConfig(
-        sample_rate=22050,
+        sample_rate=16000,
         win_length=1024,
         hop_length=256,
         num_mels=80,
@@ -34,7 +33,7 @@ def main():
 
     config = VitsConfig(
         audio=audio_config,
-        run_name="vits_ssdpt-eumember_96976",
+        run_name="vits_ssdpt-eumember_4466",
         # use_speaker_embedding=True,
         batch_size=32,
         eval_batch_size=16,
@@ -44,15 +43,14 @@ def main():
         run_eval=True,
         test_delay_epochs=-1,
         epochs=1000,
-        save_step=1000,
-        save_best_after=1000,
-        save_checkpoints=True,
-        save_n_checkpoints=4,
-        text_cleaner="portuguese_cleaners", #multilingual_cleaners
-        use_phonemes=False, #True <- helps in convergence, for now its turned off
-        phoneme_language="pt", #None
-        # phonemizer="multi_phonemizer", # Use the custom phonemizer (espeak pt)
-        phoneme_cache_path=os.path.join(output_path, "portuguese_cleaners"),
+        save_on_interrupt=True,
+        save_step=10000, # Number of training steps expected to save traning stats and checkpoints.
+        save_checkpoints=True, # If true, it saves checkpoints per "save_step"
+        text_cleaner="portuguese_cleaners", #portuguese_cleaners(basic (phonemizer does the rest)(text.replace("&", " e "))) #multilingual_cleaners(not needed for pt only)
+        use_phonemes=True, #True <- helps in convergence, for now its turned on
+        phoneme_language="pt", #None #(pt works too)
+        phonemizer="espeak", # Use the custom phonemizer(espeak)  #multi_phonemizer
+        phoneme_cache_path=os.path.join(output_path, "phoneme_cleaners"),
         compute_input_seq_cache=True,
         print_step=5,
         print_eval=True,
@@ -60,20 +58,6 @@ def main():
         output_path=output_path,
         datasets=[dataset_config],
         cudnn_benchmark=False,
-        test_sentences=[
-            [
-                "Demorei muito tempo para criar esta voz",
-                "EUmember_96976",
-                None,
-                "pt",
-            ],
-            [
-                "Frase de teste para verificar a qualidade da voz",
-                "EUmember_96976",
-                None,
-                "pt",
-            ],
-        ],
     )
 
     # INITIALIZE THE AUDIO PROCESSOR
