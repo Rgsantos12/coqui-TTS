d8ace76ee34b3fd76f79bd238c8b69a48d0b4fa0
Rgsantos12 <92592853+Rgsantos12@users.noreply.github.com>
2025-07-30 19:00:25 +0100

    organized tts recipes for ptpt

---
+++ recipes-ptpt/continue-training.bat
@@ -0,0 +1,3 @@
+@echo off
+set CUDA_VISIBLE_DEVICES=0
+python "vits/train_vits_tts_phonemes_ptpt.py" --continue_path "models/fine-tuned/one-voice/vits_ssdpt-eumember_96976-July-24-2025_06+25PM-1df082d3" 
